3127274738b18e7cfb88cae5508415e61ff400bd
minakano <michellenakano@MacBook-Air-de-Michelle.local>
2023-11-01 22:54:29 -0300

    Adição de estilo e JS.

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,65 +1,86 @@
 const state = {
-  view: {
-    squares: document.querySelectorAll(".square"),
-    enemy: document.querySelector(".enemy"),
-    timeLeft: document.querySelector("#time-left"),
-    score: document.querySelector("#score"),
-  },
-  values: {
-    gameVelocity: 1000,
-    hitPosition: 0,
-    result: 0,
-    curretTime: 60,
-  },
-  actions: {
-    timerId: setInterval(randomSquare, 1000),
-    countDownTimerId: setInterval(countDown, 1000),
-  },
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+        life: document.querySelector("#lives"),
+        retry: document.querySelector('#button')
+    },
+    values: {
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+        life: 3    
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    },
 };
 
-function countDown() {
-  state.values.curretTime--;
-  state.view.timeLeft.textContent = state.values.curretTime;
-
-  if (state.values.curretTime <= 0) {
-    clearInterval(state.actions.countDownTimerId);
-    clearInterval(state.actions.timerId);
-    alert("Game Over! O seu resultado foi: " + state.values.result);
-  }
+function toggleButton() {
+    document.getElementById("button").className = 'show'; 
 }
 
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+  
+    if (state.values.currentTime <= 0 || state.values.life <= 0) {
+      clearInterval(state.actions.countDownTimerId);
+      clearInterval(state.actions.timerId);
+      alert("GAME OVER! O seu score foi: " + state.values.result);
+      toggleButton()
+    }
+  }
+  
 function playSound(audioName) {
-  let audio = new Audio(`./src/audios/${audioName}.m4a`);
-  audio.volume = 0.2;
-  audio.play();
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
 }
 
 function randomSquare() {
-  state.view.squares.forEach((square) => {
-    square.classList.remove("enemy");
-  });
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
 
-  let randomNumber = Math.floor(Math.random() * 9);
-  let randomSquare = state.view.squares[randomNumber];
-  randomSquare.classList.add("enemy");
-  state.values.hitPosition = randomSquare.id;
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
 }
+
+function retry(){
+    state.view.retry.addEventListener('click', () => {
+      location.reload();
+    })
+  }
 
 function addListenerHitBox() {
-  state.view.squares.forEach((square) => {
-    square.addEventListener("mousedown", () => {
-      if (square.id === state.values.hitPosition) {
-        state.values.result++;
-        state.view.score.textContent = state.values.result;
-        state.values.hitPosition = null;
-        playSound("hit");
-      }
+    state.view.squares.forEach((square) => {
+      square.addEventListener("mousedown", () => {
+        if (square.id === state.values.hitPosition) {
+          state.values.result++;
+          state.view.score.textContent = state.values.result;
+          state.values.hitPosition = null;
+          playSound("hit");
+        } else if (square.id !== state.values.hitPosition && state.values.hitPosition !== null) {
+            if(state.values.life > 0){
+                playSound("explosion18")
+                state.values.life--;
+                state.view.life.textContent = 'x' + state.values.life;
+              }
+        }
+      });
     });
-  });
-}
+  }  
 
 function initialize() {
-  addListenerHitBox();
+    addListenerHitBox();
+    retry();
 }
-
+ 
 initialize();
